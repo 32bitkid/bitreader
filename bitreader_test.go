14f3a8e04f926189233fc2767da308ffc528c69d
James Holmes <32bitkid@gmail.com>
2014-06-08 21:30:13 -0400

    DRYing up tests

--- bitreader_test.go
+++ bitreader_test.go
@@ -4,63 +4,49 @@ import "testing"
 import "bytes"
 import "github.com/32bitkid/bitreader"
 
+type read32 func(uint) uint32
+
 func createReader(b ...byte) bitreader.Bitreader32 {
 	return bitreader.Create(bytes.NewReader(b))
 }
 
-func TestPeekingForZero(t *testing.T) {
-	data := []byte{0, 0, 0, 0, 0, 0, 0, 0}
-	reader := bytes.NewReader(data)
-	br := bitreader.Create(reader)
+func check32(t *testing.T, fn read32, len uint, expected uint32) {
+	if actual := fn(len); actual != expected {
+		t.Fatalf("Expected %d, got %d", expected, actual)
+	}
+}
 
+func TestPeekingForZero(t *testing.T) {
+	br := createReader(0, 0, 0, 0, 0, 0, 0, 0)
 	for i := uint(1); i < 64; i++ {
-		actual := br.Peek(i)
-		expected := uint32(0)
-		if actual != expected {
-			t.Fatalf("Expected %d, got %d", expected, actual)
-		}
+		check32(t, br.Peek, i, 0)
 	}
 }
 
 func TestPeekingForOne(t *testing.T) {
-	data := []byte{255, 255, 255, 255, 255, 255, 255, 255}
-	reader := bytes.NewReader(data)
-	br := bitreader.Create(reader)
-
+	br := createReader(255, 255, 255, 255, 255, 255, 255, 255)
 	for i := uint(1); i < 64; i++ {
-		actual := br.Peek(i)
-		expected := uint32(1<<i - 1)
-		if actual != expected {
-			t.Fatalf("Expected %d, got %d", expected, actual)
-		}
+		check32(t, br.Peek, i, uint32(1<<i-1))
 	}
 }
 
 func TestTrashingBits(t *testing.T) {
 	br := createReader(1)
 	br.Trash(7)
-	if br.Peek(1) != 1 {
-		t.Fatal("Expected to read one")
-	}
-}
-
-func check(t *testing.T, br bitreader.Bitreader32, len uint, expected uint32) {
-	if actual := br.Read(len); actual != expected {
-		t.Fatalf("Expected %d, got %d", expected, actual)
-	}
+	check32(t, br.Peek, 1, 1)
 }
 
 func TestReadingBits(t *testing.T) {
 	// 0000 0001 0000 0001 0000 0010 0000 0100
 	br := createReader(1, 1, 2, 4)
-	check(t, br, 7, 0)
-	check(t, br, 1, 1)
-	check(t, br, 7, 0)
-	check(t, br, 1, 1)
-	check(t, br, 6, 0)
-	check(t, br, 1, 1)
-	check(t, br, 6, 0)
-	check(t, br, 1, 1)
+	check32(t, br.Read, 7, 0)
+	check32(t, br.Read, 1, 1)
+	check32(t, br.Read, 7, 0)
+	check32(t, br.Read, 1, 1)
+	check32(t, br.Read, 6, 0)
+	check32(t, br.Read, 1, 1)
+	check32(t, br.Read, 6, 0)
+	check32(t, br.Read, 1, 1)
 }
 
 func TestPeekingBools(t *testing.T) {
