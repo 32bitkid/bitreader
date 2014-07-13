e6210b98b56a9a222330e8d6b9e57489c0cd71fb
James Holmes <32bitkid@gmail.com>
2014-07-13 14:07:32 -0400

    adding a buffered/blocking bitreader

---
+++ buffered_bitreader.go
@@ -0,0 +1,85 @@
+package bitreader
+
+import "io"
+import "bytes"
+
+type BufferedBitreader interface {
+	Reader32
+	Len() int
+	io.Writer
+}
+
+type bufferedBitreader struct {
+	reader Reader32
+	buffer *bytes.Buffer
+	sync   chan interface{}
+}
+
+var goToken = struct{}{}
+
+func NewBufferedBitreader() BufferedBitreader {
+	buffer := &bytes.Buffer{}
+	reader := NewReader32(buffer)
+	sync := make(chan interface{}, 1)
+	return &bufferedBitreader{reader, buffer, sync}
+}
+
+func (wbr *bufferedBitreader) Write(data []byte) (n int, err error) {
+	n, err = wbr.buffer.Write(data)
+	select {
+	case <-wbr.sync:
+		wbr.sync <- goToken
+	default:
+		wbr.sync <- goToken
+	}
+	return
+}
+
+func (wbr *bufferedBitreader) Len() int {
+	return wbr.buffer.Len()
+}
+
+func (wbr *bufferedBitreader) Trash(len uint) (err error) {
+	err = wbr.reader.Trash(len)
+	if err == ErrNotAvailable {
+		<-wbr.sync
+		err = wbr.reader.Trash(len)
+	}
+	return
+}
+
+func (wbr *bufferedBitreader) Peek32(len uint) (val uint32, err error) {
+	val, err = wbr.reader.Peek32(len)
+	if err == ErrNotAvailable {
+		<-wbr.sync
+		val, err = wbr.reader.Peek32(len)
+	}
+	return
+}
+
+func (wbr *bufferedBitreader) Read32(len uint) (val uint32, err error) {
+	val, err = wbr.reader.Read32(len)
+	if err == ErrNotAvailable {
+		<-wbr.sync
+		val, err = wbr.reader.Read32(len)
+	}
+	return
+}
+
+func (wbr *bufferedBitreader) PeekBit() (val bool, err error) {
+	val, err = wbr.reader.PeekBit()
+	if err == ErrNotAvailable {
+		<-wbr.sync
+		val, err = wbr.reader.PeekBit()
+	}
+	return
+}
+
+func (wbr *bufferedBitreader) ReadBit() (val bool, err error) {
+	val, err = wbr.reader.ReadBit()
+	if err == ErrNotAvailable {
+		<-wbr.sync
+		val, err = wbr.reader.ReadBit()
+	}
+	return
+}
